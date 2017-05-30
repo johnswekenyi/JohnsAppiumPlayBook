2374d65f2f2937a8b3fc1d0c4a2ef0f9f3ba2e83
John Swekenyi <johnswekenyi@gmail.com>
2017-05-30 22:51:01 +0300

    AndroidUIAutomator

---
+++ src/UiautomatorTest.java
@@ -0,0 +1,22 @@
+import io.appium.java_client.android.AndroidDriver;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class UiautomatorTest extends BaseClass{
+
+    AndroidDriver driver = Capabilities();
+
+    public UiautomatorTest() throws MalformedURLException {
+        driver.manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS );
+
+        //Locate element using androidUIAutomator
+        //syntax  driver.findElementByAndroidUIAutomator("attribute(\"value\")")
+        driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+        driver.findElementByAndroidUIAutomator("text(\"Animation\")").click();
+
+        //Validate clickable features for all options , syntax
+        //driver.findElementsByAndroidUIAutomator("new UiSelector().property(value)");
+        System.out.println(driver.findElementsByAndroidUIAutomator("new UiSelector().clickable(true)").size());
+    }
+}
