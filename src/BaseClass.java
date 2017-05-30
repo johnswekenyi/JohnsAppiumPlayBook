38563635568094d276e32a77902582235ce8bca2
John Swekenyi <johnswekenyi@gmail.com>
2017-05-30 15:54:07 +0300

    Added first automation script

--- src/BaseClass.java
+++ src/BaseClass.java
@@ -11,13 +11,17 @@ public class BaseClass {
     @SuppressWarnings("rawtypes")
     public static AndroidDriver driver;
 
-    public static void main (String[] args) throws MalformedURLException {
+    public static AndroidDriver Capabilities() throws MalformedURLException {
         File f = new File("src");
         File fs = new File(f, "ApiDemos-debug.apk");
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Galaxy_Nexus_android_6:5554");
         cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+        return driver;
+
+        //AndroidUIAutomator
+
     }
 }
 
