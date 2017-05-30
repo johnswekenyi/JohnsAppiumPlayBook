3a75cfa4f5aac053c3a524aa2f6811d8c069c3fa
John Swekenyi <johnswekenyi@gmail.com>
2017-05-30 12:47:29 +0300

    New commit

---
+++ src/BaseClass.java
@@ -0,0 +1,24 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseClass {
+
+    @SuppressWarnings("rawtypes")
+    public static AndroidDriver driver;
+
+    public static void main (String[] args) throws MalformedURLException {
+        File f = new File("src");
+        File fs = new File(f, "ApiDemos-debug.apk");
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Galaxy_Nexus_android_6:5554");
+        cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+
+    }
+}
+
