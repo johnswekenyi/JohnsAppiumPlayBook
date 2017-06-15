af6363b0878ec3dcddf6117e29d10389bc742b19
John Swekenyi <johnswekenyi@gmail.com>
2017-06-15 14:42:52 +0300

    Test commit with cliff

--- src/BaseClass.java
+++ src/BaseClass.java
@@ -13,15 +13,12 @@ public class BaseClass {
 
     public static void main (String[] args) throws MalformedURLException {
         File f = new File("src");
-        File fs = new File(f, "Raaga.apk");
+        File fs = new File(f, "ApiDemos-debug.apk");
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Android Emulator");
         cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
         //return driver;
-
-        //AndroidUIAutomator
-
     }
 }
 
--- src/TestFileWithCliff.java
+++ src/TestFileWithCliff.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by johnswekenyi on 15/06/2017.
  */
 public class TestFileWithCliff {
+     public static void main(String[] args){
+         System.out.println("Test file");
+     }
 }
